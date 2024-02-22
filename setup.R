e7dec600d431af20dbdbce7a012986ef7890a099
parmsam <parmartsam@gmail.com>
2024-02-22 13:41:42 -0500

    start ch20 notes

--- setup.R
+++ setup.R
@@ -1,5 +1,11 @@
 library(fs)
 
+copy_and_open <- function(from, to) {
+  fs::file_copy(from, to)
+  rstudioapi::navigateToFile(to)
+}
+
+copy_and_open("template.qmd", "20-evaluation.qmd")
 file_copy("template.qmd", "24-improving-perf.qmd")
 file_copy("template.qmd", "25-rewriting-r-code-cpp.qmd")
 file_copy("template.qmd", "00-useful-resources.qmd")
